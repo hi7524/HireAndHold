d2d6d73ae6a0c63d5a97d893e699554b5ae1a5dc
김혜인 <rjqnrdl7524@naver.com>
2025-11-07 17:59:41 +0900

    create volume mixers

--- Assets/Scripts/Defines.cs
+++ Assets/Scripts/Defines.cs
@@ -5,8 +5,14 @@ public static class DataTableIds
     public static readonly string String = "StringTable";
 }
 
-
 public static class Tags
 {
     // public static readonly string GameController = "GameController";
+}
+
+public static class AudioMixerParams
+{
+    public static readonly string Master = "Master";
+    public static readonly string Bgm = "BGM";
+    public static readonly string Sfx = "SFX";
 }
