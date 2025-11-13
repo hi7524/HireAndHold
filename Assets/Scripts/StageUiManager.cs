2643f7dc57bd5651b45f1b8e38b9f0a18c56aa83
김혜인 <rjqnrdl7524@naver.com>
2025-11-13 21:21:08 +0900

    Refactor game architecture with separation of concerns

--- Assets/Scripts/StageUiManager.cs
+++ Assets/Scripts/StageUiManager.cs
@@ -1,11 +1,31 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class StageUiManager : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
     [Space]
-    [SerializeField] private GameObject levelUpCardObj;
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private TextMeshProUGUI speedLevelText;
 
-    
+    private void Update()
+    {
+        if (!gameManager.IsGameStarted)
+            return;
 
+        UpdateTimerText(gameManager.ElapsedTime);
+    }
+
+    private void UpdateTimerText(float elapsedTime)
+    {
+        int minutes = Mathf.FloorToInt((elapsedTime % 3600) / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+
+        timerText.text = $"{minutes:00}:{seconds:00}";
+    }
+
+    public void UpdateSpeedLevelText()
+    {
+        speedLevelText.text = $"X{gameManager.CurSpeedLevel}";
+    }
 }
