a6e34b4fa7a64d24e7f9a518a2943237d1e05e81
김혜인 <rjqnrdl7524@naver.com>
2025-11-17 17:56:36 +0900

    add visual grid preview during unit drag

--- Assets/Scripts/DevScripts_HI/GridUnit.cs
+++ Assets/Scripts/DevScripts_HI/GridUnit.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 public class GridUnit : MonoBehaviour, ITestDraggable
 {
     [SerializeField] private UnitGridData gridData;
+    [SerializeField] private float cellSize = 0.6f;
+    [SerializeField] private GameObject cellPrf;
 
     // 그리드 데이터
     public UnitGridData GridData => gridData;
@@ -16,20 +19,33 @@ public class GridUnit : MonoBehaviour, ITestDraggable
     private GridCell curGridCell;
     private GridCell previousGridCell;
 
+    private List<Transform> childrenObj = new List<Transform>();
+
+
+    private void Start()
+    {
+        CreatePreviewSprites();
+    }
 
     public void OnDragStart()
     {
+        // GridCell 설정 관련
         previousGridCell = curGridCell;
         curGridCell?.ClearObject();
         curGridCell = null;
+
+        // Grid 정보 시각화(미리보기) 관련
+        SetActiveChildrenObj(true);
     }
 
     public void OnDrag()
     {
+        
     }
 
     public void OnDragEnd()
     {
+        SetActiveChildrenObj(false);
     }
 
     public void OnDropFailed()
@@ -55,5 +71,42 @@ public class GridUnit : MonoBehaviour, ITestDraggable
     public GridCell GetPreviousCell()
     {
         return previousGridCell;
+    }
+
+    // 미리보기 스프라이트 생성
+    private void CreatePreviewSprites()
+    {
+        if (gridData == null || cellPrf == null)
+            return;
+
+        for (int i = 0; i < gridData.GetOccupiedCells().Count; i++)
+        {
+            // 생성
+            var cell = Instantiate(cellPrf, transform);
+            childrenObj.Add(cell.transform);
+            cell.transform.localScale = cellSize * Vector3.one;
+
+            // 위치 설정
+            Vector2Int cellPos = gridData.GetOccupiedCells()[i];
+            cell.transform.localPosition = new Vector3(cellPos.x * cellSize, cellPos.y * cellSize, 0);
+        }
+
+        var center = Instantiate(cellPrf, transform);
+        childrenObj.Add(center.transform);
+        center.transform.localScale = cellSize * Vector3.one;
+
+        center.transform.localPosition = transform.position;
+    }
+
+    // 자식 오브젝트 전체 비활성화 및 활성화
+    private void SetActiveChildrenObj(bool value)
+    {
+        if (childrenObj == null || childrenObj.Count == 0)
+            return;
+
+        for (int i = 0; i < childrenObj.Count; i++)
+        {
+            childrenObj[i].gameObject.SetActive(value);
+        }
     }
 }
