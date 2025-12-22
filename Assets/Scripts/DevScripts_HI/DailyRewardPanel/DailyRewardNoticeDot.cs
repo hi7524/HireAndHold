4a849575c5e6636176ffba1c3bf27b75ef6c829f
김혜인 <rjqnrdl7524@naver.com>
2025-12-22 10:54:01 +0900

    add notice dot

---
+++ Assets/Scripts/DevScripts_HI/DailyRewardPanel/DailyRewardNoticeDot.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class DailyRewardNoticeDot : MonoBehaviour
+{
+    [SerializeField] private GameObject noticeDotObj;
+
+    private async void Start()
+    {
+        // DatabaseManager 초기화 대기
+        if (DatabaseManager.Instance != null)
+        {
+            await DatabaseManager.Instance.WaitForInitializationAsync();
+            UpdateNoticeDot();
+        }
+        else
+        {
+            if (noticeDotObj != null)
+                noticeDotObj.SetActive(false);
+        }
+    }
+
+    public void UpdateNoticeDot()
+    {
+        if (noticeDotObj == null)
+        {
+            return;
+        }
+
+        if (DatabaseManager.Instance == null)
+        {
+            noticeDotObj.SetActive(false);
+            return;
+        }
+
+        // 오늘 보상을 받았는지 체크
+        bool hasClaimed = DatabaseManager.Instance.HasClaimedToday();
+        noticeDotObj.SetActive(!hasClaimed);
+    }
+}
